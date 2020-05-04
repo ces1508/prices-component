2d11d656df889c95f4bcae0acccf6f50972948ba
Christian Segura <ces1508@gmail.com>
2020-05-03 20:45:03 -0500

    Updates

---
+++ precache-manifest.819b258edff19c145af1cf0fecd045f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81795bbe051a167110fda0101d2183d3",
+    "url": "/prices-component/index.html"
+  },
+  {
+    "revision": "590fb92666ac4298b62d",
+    "url": "/prices-component/static/css/main.9956bc79.chunk.css"
+  },
+  {
+    "revision": "f177f2fc1c74dbde585a",
+    "url": "/prices-component/static/js/2.965bf10c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prices-component/static/js/2.965bf10c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "590fb92666ac4298b62d",
+    "url": "/prices-component/static/js/main.4b011eec.chunk.js"
+  },
+  {
+    "revision": "9cb7102425c36025e64b",
+    "url": "/prices-component/static/js/runtime-main.710b73a5.js"
+  }
+]);
